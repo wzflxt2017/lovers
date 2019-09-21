9f6cf3e90c1a32af440775ec8b38aef6c19d7879
wangzefengMac <wzflxt@163.com>
2019-09-21 13:38:10 +0800

    first commit

---
+++ src/test/java/com/lovers/java/LoversApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lovers.java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoversApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
