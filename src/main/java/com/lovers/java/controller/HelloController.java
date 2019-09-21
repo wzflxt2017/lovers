20d689d6d0c287d9447a1aa7ed81ab088ca8e46d
wangzefengMac <wzflxt2017@163.com>
2019-09-21 17:20:38 +0800

    jquery and bootstrap

--- src/main/java/com/lovers/java/controller/HelloController.java
+++ src/main/java/com/lovers/java/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.lovers.java.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
-    public String hello(@RequestParam(name = "name")String name, ModelAndView modelAndView){
-        modelAndView.addObject("name",name);
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        String name="wangzefeng";
+        model.addAttribute("name",name);
         return "hello";
     }
 
