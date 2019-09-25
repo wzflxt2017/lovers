e1f32e8608b0a6ae3ae8851c2a7aa49c7caee732
wangzefengMac <wzflxt2017@163.com>
2019-09-25 11:02:23 +0800

    mybatis generator生成代码

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -10,13 +10,13 @@ create table sys_user
 	user_level int(2) comment '用户等级',
 	user_role varchar(20) comment '用户在家庭的角色',
 	user_email varchar(30) comment '用户邮箱',
-	user_birthday timestamp(3) comment '用户生日',
+	user_birthday timestamp (3) null comment '用户生日',
 	user_address varchar(50) comment '用户地址',
 	user_location varchar(50) comment '用户所在地',
 	user_photo int(10) comment '用户头像',
 	user_state int(2) comment '用户状态:1为可用，2为不可用',
-	create_time timestamp(3) comment '创建时间',
-	modified_time timestamp(3) comment '最后一次修改时间',
+	create_time timestamp (3) null comment '创建时间',
+	modified_time timestamp (3) null comment '最后一次修改时间',
 	constraint sys_user_pk
 		primary key (user_id)
 )
@@ -31,7 +31,7 @@ create table sys_file(
 	file_full_name varchar (50) comment '文件全名',
 	for_module varchar (20) comment '所属模块',
 	user_id int(10) comment '上传文件的用户',
-	upload_time timestamp (3) comment '上传时间',
+	upload_time timestamp (3) null comment '上传时间',
 	constraint sys_file_pk
 		primary key (file_id)
 
@@ -57,7 +57,7 @@ create table user_message(
   is_read int(2) comment '是否已读',
   message_file_name varchar (50) comment '文件名',
   message_file_id int(10) comment '文件id',
-  message_time timestamp (3) comment '消息发送时间',
+  message_time timestamp (3) null comment '消息发送时间',
   constraint user_message_pk
    primary key(message_id)
 )
@@ -69,8 +69,8 @@ album_name varchar (50) comment '相册名',
 album_cover int(10) comment '相册封面',
 album_describe varchar (500) comment '相册描述',
 user_id int(10) comment '创建者id',
-create_time timestamp (3) comment '相册创建时间',
-modified_time timestamp (3) comment '最后一次修改时间',
+create_time timestamp (3) null comment '相册创建时间',
+modified_time timestamp (3) null comment '最后一次修改时间',
   constraint user_photo_album_pk
    primary key(album_id)
 )
@@ -82,7 +82,7 @@ album_id int(10) comment '相册id',
 photo_name varchar (50) comment '图片名称',
 photo_position varchar (400) comment '图片位置',
 photo_describe varchar (500) comment '图片描述',
-upload_time timestamp (3) comment '上传时间',
+upload_time timestamp (3) null comment '上传时间',
 user_id int(10) comment '上传者id',
 for_module varchar (50) comment '所属模块',
   constraint user_photo_pk
@@ -96,7 +96,7 @@ record_title varchar (50) comment '标题',
 record_images varchar(200) comment '多张图片id，以逗号分割',
 record_content varchar (4000) comment '正文',
 user_id int(10) comment '上传者id',
-create_time timestamp (3) comment '记录时间',
+create_time timestamp (3) null comment '记录时间',
 is_hidden int(2) comment '是否隐藏',
 visitor_number int(10) comment '访问数量',
 comment_number int(10) comment '评论数量',
@@ -111,7 +111,7 @@ comment_id int(10) auto_increment,
 record_id int(10) comment '心情记录id',
 comment_content varchar(1000) comment '正文',
 user_id int(10) comment '评论人id',
-comment_time timestamp (3) comment '评论时间',
+comment_time timestamp (3) null comment '评论时间',
   constraint user_mood_comment_pk
    primary key(comment_id)
 )
@@ -121,8 +121,8 @@ create table lovers_incident(
 incident_id int(10) auto_increment,
 incident_title varchar (50) comment '标题',
 incident_content varchar (4000) comment '正文',
-incident_time timestamp (3) comment '事件发生时间',
-incident_create_time timestamp (3) comment '记录创建时间',
+incident_time timestamp (3) null comment '事件发生时间',
+incident_create_time timestamp (3) null comment '记录创建时间',
 user_id_man int(10) comment '情侣中男id',
 user_id_woman int(10) comment '情侣中女id',
 user_id_create int(10) comment '创建人id',
@@ -139,7 +139,7 @@ user_id_man int(10) comment '情侣中男id',
 user_id_woman int(10) comment '情侣中女id',
 space_background int(10) comment '背景图片id',
 space_content varchar (1000) comment '正文',
-create_time timestamp (3) comment '空间创建时间',
+create_time timestamp (3) null comment '空间创建时间',
   constraint lovers_space_pk
    primary key(space_id)
 )
@@ -182,7 +182,7 @@ create table workflow_business(
 business_id int(10) auto_increment,
 workflow_id int(10),
 data_id int(10),
-create_time timestamp (3) comment '创建时间',
+create_time timestamp (3) null comment '创建时间',
 create_user_id int(10),
 create_user_name varchar(20) comment '用户昵称',
 status int(2) comment '状态',
@@ -197,10 +197,10 @@ task_id int(10) auto_increment,
 business_id int(10),
 start_user_id int(10),
 start_user_name varchar(20) comment '用户昵称',
-start_time timestamp (3) comment '任务开始时间',
+start_time timestamp (3) null comment '任务开始时间',
 end_user_id int(10),
 end_user_name varchar(20) comment '用户昵称',
-end_time timestamp (3) comment '任务结束时间',
+end_time timestamp (3) null comment '任务结束时间',
 node_id int(10),
 link_id: int(10),
 status int(2) comment '状态',
