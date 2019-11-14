2430da9efb708c2ad29151163437a1a2b10293d0
wangzefengWindows <wzflxt2017@163.com>
2019-11-14 10:43:26 +0800

    dsad

---
+++ src/test/java/com/lovers/java/Demo.java
@@ -0,0 +1,12 @@
+package com.lovers.java;
+
+/**
+ * Created by wangzefeng on 2019/11/8 0008.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        String s="大萨达多";
+        System.out.println(s.indexOf("大"));
+    }
+}
