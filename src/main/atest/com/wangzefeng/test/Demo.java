db9ea5e239f897cf5f2dabde233efb01745c9e45
wangzefengWindows <wzflxt2017@163.com>
2019-12-30 15:04:00 +0800

    消息模块

--- src/main/atest/com/wangzefeng/test/Demo.java
+++ src/main/atest/com/wangzefeng/test/Demo.java
@@ -1,5 +1,8 @@
 package com.wangzefeng.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Auther: wangzefeng
  * @Date: 2019-10-28 15:31
@@ -9,12 +12,16 @@ public class Demo {
 
     public static void main(String[] args) {
         String s1="hi";
-        modi(s1);
-        System.out.println(s1);
+        List<String> list=new ArrayList<>();
+        list.add(s1);
+        modi(s1,list);
+        System.out.println(s1+list);
     }
 
-    public static void modi(String s1){
+    public static void modi(String s1,List<String> list){
         s1="hello";
+        list=new ArrayList<>();
+        list.add(s1);
     }
 
     class Student{
